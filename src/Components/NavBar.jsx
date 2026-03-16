ace6af42c218ae05a81c22c8a9d5b094c3bac624
gowtham16072002@gmail.com <gowtham16072002@gmail.com>
2026-03-16 11:36:53 +0530

    Removing the comments for home and about page

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -13,69 +13,35 @@ function NavBar() {
 
   return (
     <div>
-      {/* <nav className="navbar">
-        <div className='imageblock'>
-
-
-          <div className="logo">
-            <img
-              src={logo}
-              alt="VictoryFit Logo"
-              className="logo-img"
-            />
-          </div>
-          <div className='logoName'>
-            <img src={logoName}
-              alt="VictoryFit Logo"
-              className="logoName-img"
-            />
-          </div>
-        </div>
-        <ul className="nav-links">
-
-          <li><Link className='Nav-route' to="/">Home</Link></li>
-          <li><Link className='Nav-route' to="/about">About</Link></li>
-          <li><Link className='Nav-route' to="/service">Services</Link></li>
-          <li><Link className='Nav-route' to="/programs">Programs</Link></li>
-          <li><Link className='Nav-route' to="/contact">Contact</Link></li>
-
-        </ul>
-        <Link to="/contact" className="btn primary nav-button"><b>Get started</b></Link>
-      </nav> */}
-
-
       <nav className="navbar">
 
-      {/* Logo section */}
-      <div className='imageblock'>
-        <div className="logo">
-          <img src={logo} alt="VictoryFit Logo" />
+        <div className='imageblock'>
+          <div className="logo">
+            <img src={logo} alt="VictoryFit Logo" />
+          </div>
+
+          <div className='logoName'>
+            <img src={logoName} alt="VictoryFit Logo" />
+          </div>
         </div>
 
-        <div className='logoName'>
-          <img src={logoName} alt="VictoryFit Logo"/>
+        <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+          ☰
         </div>
-      </div>
 
-      {/* Hamburger icon */}
-      <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
-        ☰
-      </div>
+        <ul className={menuOpen ? "nav-links active" : "nav-links"}>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/service">Services</Link></li>
+          <li><Link to="/programs">Programs</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
 
-      {/* Navigation links */}
-      <ul className={menuOpen ? "nav-links active" : "nav-links"}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/service">Services</Link></li>
-        <li><Link to="/programs">Programs</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
+        <Link to="/contact" className="btn primary nav-button">
+          Get Started
+        </Link>
 
-      <Link to="/contact" className="btn primary nav-button">
-        Get Started
-      </Link>
-
-    </nav>
+      </nav>
     </div>
   )
 }
